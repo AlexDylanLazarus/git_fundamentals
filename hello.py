256aa2680656820575375dda841f2a40203f644c
AlexDylanLazarus <alexdylanlazarus@gmail.com>
2024-03-06 09:18:01 +0200

    added a double function

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("Hello")
+
+
+def double(n):
+    return n * 2
+
+
+# git init creates a folder that'll track everything like changes etc.
+# type explorer .
+# then click view show hidden items to see folder
+
+# to stage from command line
+# git add . for all files
+# git add file name just for that file
+
+# git commit -m "This is my first project with git"
+# to see commits you've done so far
